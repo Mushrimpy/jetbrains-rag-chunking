9e251a13915d6185bd7f8b318c5bff801d5dfddf
Moncef Slimani <moncef.slimani.ms@gmail.com>
2025-04-08 03:54:32 +0100

    Add hyperparameter tuning

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+import pandas as pd
 from fixed_token_chunker import FixedTokenChunker
 from pipeline import run_evaluation
 from sentence_transformers import SentenceTransformer
@@ -13,19 +15,37 @@ def create_embedder(model_name="sentence-transformers/multi-qa-mpnet-base-dot-v1
 
 
 def main():
+    os.makedirs("results", exist_ok=True)
 
-    print("\nEnter the following hyperparameters:")
+    chunk_sizes = [200, 400, 600]
+    chunk_overlaps = [0, 50, 100]
+    retrieval_counts = [2, 5, 10]
+    embedding_func = create_embedder()
 
-    chunk_size = int(input("Chunk size: "))
-    chunk_overlap = int(input("Chunk overlap: "))
-    num_retrievals = int(input("Number of retrievals: "))
+    results_data = []
 
-    print("\nRunning Evaluation:")
-    embedding_fn = create_embedder()
-    chunker = FixedTokenChunker(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    results = run_evaluation(chunker, embedding_fn, num_retrievals)
-    print(f"Average Precision: {results['avg_precision']:.4f}")
-    print(f"Average Recall: {results['avg_recall']:.4f}")
+    for chunk_size in chunk_sizes:
+        for chunk_overlap in chunk_overlaps:
+            for retrieval_count in retrieval_counts:
+                print(
+                    f"\nRunning Evaluation with chunk_size={chunk_size}, chunk_overlap={chunk_overlap}, retrieval_count={retrieval_count}:"
+                )
+                chunker = FixedTokenChunker(
+                    chunk_size=chunk_size, chunk_overlap=chunk_overlap
+                )
+                results = run_evaluation(chunker, embedding_func, retrieval_count)
+                results_data.append(
+                    {
+                        "chunk_size": chunk_size,
+                        "chunk_overlap": chunk_overlap,
+                        "num_retrievals": retrieval_count,
+                        "precision": f"{results['avg_precision']:.4f}",
+                        "recall": f"{results['avg_recall']:.4f}",
+                    }
+                )
+
+    results_df = pd.DataFrame(results_data)
+    results_df.to_csv("results/summary.csv")
 
 
 if __name__ == "__main__":
--- src/pipeline.py
+++ src/pipeline.py
@@ -6,8 +6,8 @@ from utils import load_corpus, load_questions
 
 def run_evaluation(
     chunker,
-    embedding_fn,
-    num_retrievals,
+    embedding_func,
+    retrieval_count,
     corpus_path="data/state_of_the_union.md",
     questions_path="data/questions_df.csv",
 ):
@@ -17,20 +17,20 @@ def run_evaluation(
 
     chunks = chunker.split_text(corpus)
     chunk_objects = [{"text": chunk, "id": i} for i, chunk in enumerate(chunks)]
-    chunk_embeddings = np.array([embedding_fn(chunk) for chunk in chunks])
+    chunk_embeddings = np.array([embedding_func(chunk) for chunk in chunks])
 
     results = {"precision": [], "recall": []}
 
     for _, row in questions_df.iterrows():
         query = row["question"]
         golden_references = row["references"]
-        query_embedding = embedding_fn(query)
+        query_embedding = embedding_func(query)
 
         # Retrival by cosine similarity
         similarities = np.dot(chunk_embeddings, query_embedding) / (
             np.linalg.norm(chunk_embeddings, axis=1) * np.linalg.norm(query_embedding)
         )
-        top_indices = np.argsort(similarities).flatten()[-num_retrievals:]
+        top_indices = np.argsort(similarities).flatten()[-retrieval_count:]
         retrieved_chunks = [chunk_objects[i] for i in top_indices]
 
         metrics = calculate_metrics(retrieved_chunks, golden_references, corpus)
